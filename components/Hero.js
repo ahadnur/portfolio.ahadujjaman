2343a89b27cff4a6eec9e1e5bee47f39f5bba9f1
ahadnur <ahadnur44@gmail.com>
2022-06-07 19:10:04 +0600

    apos changed

--- components/Hero.js
+++ components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
                 <h1 className="text-4xl font-bold text-white/90 sm:text-5xl md:text-6xl lg:text-[80px]">Ahadujjaman Nur.</h1>
                 <h2 className="text-4xl font-bold text-white/60 sm:text-5xl md:text-6xl lg:text-[80px]">I build things for the web.</h2>
             </div>
-            <p className="text-white/60 w-full md:w-[570px] my-5 md:my-8 lg:my-10">I am a Freelance Web developer specialized in Frontend development <span className="text-main">(Design and Develop)</span> and Programming Coach <span className="text-main"> (Python, Javascript, Web design and development)</span>. Currently I'm focusing on serving web solutions at <span className="text-main">Fiverr</span>, <span className="text-main">Upwork</span> </p>
+            <p className="text-white/60 w-full md:w-[570px] my-5 md:my-8 lg:my-10">I am a Freelance Web developer specialized in Frontend development <span className="text-main">(Design and Develop)</span> and Programming Coach <span className="text-main"> (Python, Javascript, Web design and development)</span>. Currently I&apos;m focusing on serving web solutions at <span className="text-main">Fiverr</span>, <span className="text-main">Upwork</span> </p>
             <button className="mt-5 py-3 px-5 border-[1px] border-main rounded-md text-main hover:bg-main/10 transition-all duration-300">
                 <Link href="https://github.com">
                     <a>Check out my course</a>
