490fa2a14982facbc71d7d7a984eddfd4e7b86de
ahadnur <ahadnur44@gmail.com>
2022-06-08 01:04:05 +0600

    doc updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 // pages/_document.js
-import { Html, Head, Main } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
@@ -11,6 +11,7 @@ export default function Document() {
       </Head>
       <body>
         <Main />
+        <NextScript />
       </body>
     </Html>
   )
