12b4148608dc795c690f3b8a55a3016a11985aa4
ahadnur <ahadnur44@gmail.com>
2022-06-09 23:02:00 +0600

    constants file updated

--- constants/constants.js
+++ constants/constants.js
@@ -15,7 +15,7 @@ export const featured_projects = [
         image: '/img/project_thumb/fp2.png',
         url: 'https://www.islamicsocietywf.org/',
         github: '',
-        tags: ['Next Js', 'Tailwind Css', 'Vercel', 'Emailjs', 'Sanity CMS']
+        tags: ['Next Js', 'Tailwind Css', 'Vercel', 'Emailjs', 'Sanity']
     },
     {
         id: 2,
@@ -24,11 +24,184 @@ export const featured_projects = [
         image: '/img/project_thumb/fp3.png',
         url: 'https://cspc.vercel.app/',
         github: 'https://github.com/ahadnur/cspc',
-        tags: ['Next Js', 'Tailwind Css', 'Vercel', 'Sanity CMS']
+        tags: ['Next Js', 'Tailwind Css', 'Vercel', 'Sanity']
     },
     
 ]
 
 export const noteable_projects = [
+    {
+        id: 0,
+        title: "Integrate algolia search with Next.js",
+        description: "A very simple blog app for a furniture store with next js sanity and algolia.",
+        year: 2022,
+        url: "https://furniture-haven.vercel.app/",
+        github: "https://github.com/ahadnur/furniture-haven",
+        tags: ['Next js', 'Tailwind Css', 'Sanity', 'Algolia']
+    },
+    {
+        id: 1,
+        title: "Auth UI with Next js",
+        description: "Material Design Authentication User interface made with Next js.",
+        year: 2022,
+        url: "https://auth-ui-next-js.vercel.app/",
+        github: "https://github.com/ahadnur/auth-ui-next.js",
+        tags: ['Next js', 'MDB']
+    },
+    {
+        id: 2,
+        title: "Netflix Clone",
+        description: "Very simple clone of Netflix with React js, Firebase and TMDB api.",
+        year: 2021,
+        url: "https://netflixxx-clone.firebaseapp.com/",
+        github: "https://github.com/ahadnur/nf__clone",
+        tags: ['React js', 'Css', 'Firebase', 'TMDB']
+    },
+    
+    {
+        id: 3,
+        title: "Previous Portfolio",
+        description: "My previous animated Portfolio made up with Html Css and Javascript",
+        year: 2021,
+        url: "https://ahadnur.github.io/animated-port/",
+        github: "https://github.com/ahadnur/animated-port",
+        tags: ['Html', 'Css', 'JavaScript']
+    },
+    {
+        id: 4,
+        title: "TOFES4",
+        description: "A construction company website made up with react js.",
+        year: 2021,
+        url: "",
+        github: "https://github.com/ahadnur/tofes4",
+        tags: ['React js', 'MDB', 'Styled Components']
+    },
+    {
+        id: 5,
+        title: "Javascript Quiz App",
+        description: "Very simple multiple choice Programming Quiz app made up with Html, Css and Javascript",
+        year: 2021,
+        url: "",
+        github: "https://github.com/ahadnur/quizapp",
+        tags: ['Html', 'Css', 'Javascript']
+    },
+    {
+        id: 6,
+        title: "Charitable Showcase",
+        description: "Website for Khulna Ahsaniya Mohila Mission which is a Charitable organization which helps poop women.",
+        year: 2021,
+        url: "https://khahmomission.herokuapp.com/",
+        github: "https://github.com/ahadnur/kamm",
+        tags: ['Html', 'Css', 'Javascript', 'Django']
+    },
+    {
+        id: 7,
+        title: "Ecommerce Template",
+        description: "Responsive Ecommerce Template for small type business",
+        year: 2021,
+        url: "",
+        github: "https://github.com/ahadnur/yousri-ecom",
+        tags: ['Html', 'Css', 'Javascript', 'MySql']
+    },
+    {
+        id: 8,
+        title: "Git Automation",
+        description: "Automative command line app to push things to github.",
+        year: 2021,
+        url: "",
+        github: "https://github.com/ahadnur/git_auto",
+        tags: ['Python']
+    },
+    {
+        id: 9,
+        title: "Random User",
+        description: "A random user app,  data fetching from jsonplaceholder with vue js.",
+        year: 2020,
+        url: "",
+        github: "https://github.com/ahadnur/random_user_with_vuejs",
+        tags: ['Vue Js']
+    },
+    {
+        id: 10,
+        title: "Portfolio for Rachelle",
+        description: "A very simple responsive portfolio for Rachelle with Html, Css and JavaScript",
+        year: 2020,
+        url: "",
+        github: "https://github.com/ahadnur/rachelle",
+        tags: ['Html', 'Css', 'Javascript']
+    },
+    {
+        id: 11,
+        title: "Sqlcamp",
+        description: "Landing page for sqlcam a canada based coding camp.",
+        year: 2020,
+        url: "",
+        github: "https://github.com/ahadnur/sqlcamp",
+        tags: ['Html', 'Css', 'Javascript']
+    },
+    {
+        id: 12,
+        title: "Animated Landing",
+        description: "Watch marketing landind page with animation.",
+        year: 2020,
+        url: "https://ahadnur.github.io/animatied-web/",
+        github: "https://github.com/ahadnur/animatied-web",
+        tags: ['Html', 'Css', 'Javascript']
+    },
+    {
+        id: 13,
+        title: "Landing Page",
+        description: "Landing page template, made up with Html, Css, Js.",
+        year: 2020,
+        url: "https://ahadnur.github.io/responsive-web/",
+        github: "https://github.com/ahadnur/responsive-web",
+        tags: ['Html', 'Css', 'Javascript']
+    },
+    {
+        id: 14,
+        title: "Poll App",
+        description: "Simple Poll App made up with Django, Html, Css, JavaScript",
+        year: 2020,
+        url: "",
+        github: "https://github.com/ahadnur/django-poll-app",
+        tags: ['Django', 'Html', 'Css', 'Javascript']
+    },
+    {
+        id: 15,
+        title: "Rest Api",
+        description: "Created Api project for fun with django rest framework.",
+        year: 2019,
+        url: "",
+        github: "https://github.com/ahadnur/dj_rest_framework",
+        tags: ['Python', 'Django']
+    },
+    {
+        id: 16,
+        title: "Console Game",
+        description: "Snake Game in console with python.",
+        year: 2019,
+        url: "",
+        github: "https://github.com/ahadnur/snake_py",
+        tags: ['Python']
+    },
+    {
+        id: 17,
+        title: "BOT Development",
+        description: "Bot for twitter to tweet from a paragraph from a pdf until it's finished.",
+        year: 2019,
+        url: "",
+        github: "https://github.com/ahadnur/twitter-bot",
+        tags: ['Python', 'Heroku']
+    },
+    {
+        id: 18,
+        title: "Fun Project",
+        description: "Practicing python topics and made a Python console program similar to banking.",
+        year: 2018,
+        url: "",
+        github: "https://github.com/ahadnur/bkash",
+        tags: ['Python']
+    },
+
     
 ]
