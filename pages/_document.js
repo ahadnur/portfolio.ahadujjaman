e5a761e39517c964585839d0e4dd4a3dbd522e54
ahadnur <ahadnur44@gmail.com>
2022-07-17 18:49:35 +0600

    seo updated final

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import SEO from '@bradgarropy/next-seo'
 
 export default function Document() {
   return (
@@ -8,7 +8,11 @@ export default function Document() {
       <Head>
         {/* Change your Icon */}
         <link rel="icon" href="/img/logo.svg" />
-        
+        <SEO
+          title="Web Designer, Developer, and Programming Coach"
+          description="It's Ahadujjaman Nur a professional Freelance Web Designer and Developer. I offer services in Web Design, Web Development, and Programming. I also offer coaching in HTML, CSS, TailwindCss JavaScript, Python, React, Nextjs. I also offer consulting services in Web Design, Web Development, and Programming with Python."
+          keywords={['Website', 'Web development', 'Web design', 'Html', 'CSS', 'Javascript', 'TailwindCss', 'React', 'Nextjs', 'UI/UX', 'Frontend Development', 'Web Service', 'Coaching', 'Teaching', 'Assignment', 'tests']}
+        />
         
       </Head>
       <body>
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ import Link from 'next/link'
 import { AiFillGithub, AiOutlineTwitter, AiFillLinkedin, AiFillFacebook, AiFillYoutube } from 'react-icons/ai'
 import { FaTelegramPlane } from 'react-icons/fa'
 
-import SEO from '@bradgarropy/next-seo'
-
 import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 import About from '../components/About'
@@ -19,12 +17,9 @@ export default function Home() {
   return (
     <>
         <Head>
+            <title>Ahadujjamn Nur</title>
+            <meta name="description" content="Ahadujjaman Nur | Freelance Web developer | Programming Coach" />
             <link rel="icon" href="/favicon.png" />
-            <SEO
-              title="Ahadujjaman Nur | Web Designer | Developer | Programming Coach"
-              description="It's Ahadujjaman Nur a professional Freelance Web Designer and Developer. I offer services in Web Design, Web Development, and Programming. I also offer coaching in HTML, CSS, TailwindCss JavaScript, Python, React, Nextjs. I also offer consulting services in Web Design, Web Development, and Programming with Python."
-              keywords={['Website', 'Web development', 'Web design', 'Html', 'CSS', 'Javascript', 'TailwindCss', 'React', 'Nextjs', 'UI/UX', 'Frontend Development', 'Web Service', 'Coaching', 'Teaching', 'Assignment', 'tests']}
-            />
         </Head>
 
         <Nav />
