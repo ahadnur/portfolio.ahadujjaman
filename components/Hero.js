387a8c6651da2d3eef3177070024b63ef935c0cc
ahadnur <ahadnur44@gmail.com>
2022-09-15 11:16:33 +0600

    course page link updated

--- components/Hero.js
+++ components/Hero.js
@@ -17,8 +17,8 @@ const Hero = () => {
             </motion.div>
             <p className="text-white/60 w-full md:w-[570px] my-5 md:my-8 lg:my-10">I am a Freelance Web developer specialized in Frontend development <span className="text-main">(Design and Develop)</span> and Programming Coach <span className="text-main"> (Python, Javascript, Web design and development)</span>. Currently I&apos;m focusing on serving web solutions at <span className="text-main"><Link href="https://www.fiverr.com/ahadnur44?public_mode=true">Fiverr</Link></span>, <span className="text-main"><Link href="https://www.upwork.com/freelancers/~01ba501685672fd7a0?viewMode=1">Upwork</Link></span> </p>
             <button className="mt-5 py-3 px-5 border-[1px] border-main rounded-md text-main hover:bg-main/10 transition-all duration-300">
-                <Link href="https://www.youtube.com/channel/UCen4h4v9NnDKEHzOvE93BIA" >
-                    <a target="_blank">Check out my Youtube Channel</a>
+                <Link href="https://psylab.teachable.com/p/beginners-javascript-guide" >
+                    <a target="_blank">Check out my JavaScript Course</a>
                 </Link>
             </button>
         </div>
